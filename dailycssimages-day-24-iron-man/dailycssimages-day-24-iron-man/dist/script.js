4a17d0208435b807dc508eb5980ea251697b8a70
Shubham Pitekar <65482186+its-shubham2323@users.noreply.github.com>
2022-01-23 21:31:07 +0530

    commit

---
+++ dailycssimages-day-24-iron-man/dailycssimages-day-24-iron-man/dist/script.js
@@ -0,0 +1,4 @@
+/*
+Mainly inspired by this drawing videotutorial for kids https://www.youtube.com/watch?v=9MB1S6Y_VQo (I kinda suck drawing xD). Thank you so much, Draw so cute :D.
+Also a bit by a drawn in becuo.com. Didn't put the photo because Firefox warn the site is a pishing site ^_^U, I saw it in DuckDuckGo image results for "iron man + chibi". I gave me the idea about how to colour arms & legs.
+*/
